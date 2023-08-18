b6431b82d5e495a9cad5660d6adf3d8f34320e14
abhinav1912 <abhinavmathur1912@gmail.com>
2023-08-18 20:20:30 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Swift-Image-Analyser
---
+++ SwiftImageAnalyser/SwiftImageAnalyserApp.swift
@@ -0,0 +1,12 @@
+//
+
+import SwiftUI
+
+@main
+struct SwiftImageAnalyserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
